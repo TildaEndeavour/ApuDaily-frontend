a2ea0bd89277184f9cc15841201a91620fb4d144
TildaEndeavour <bardakoff.v@gmail.com>
2025-08-26 13:34:12 +0300

    ErrorPage added

---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,25 @@
+import {isRouteErrorResponse, useRouteError} from "react-router-dom";
+
+const ErrorPage = () => {
+
+    const error = useRouteError();
+
+    let title = 'An error occurred!';
+    let message = 'Something went wrong!';
+
+    if (isRouteErrorResponse(error)) {
+        title = 'Failed to load posts';
+        message = error.statusText;
+    } else if (error instanceof Error) {
+        message = error.message;
+    }
+
+    return(
+        <div className="h-48 w-96 border-gray-200 border rounded-3xl shadow-2xl bg-red-100 flex flex-col justify-center items-center mt-24">
+            <h1 className="font-bold">{title}</h1>
+            <p>{message}</p>
+        </div>
+    );
+}
+
+export default ErrorPage;
