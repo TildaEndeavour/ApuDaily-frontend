163c47c4ecdd96913df8b2d1f47e062bf1215314
TildaEndeavour <bardakoff.v@gmail.com>
2025-09-27 11:40:29 +0300

    Post Editor Alignment

--- src/publication/components/Editor.tsx
+++ src/publication/components/Editor.tsx
@@ -117,8 +117,8 @@ const QuillEditor: React.FC<QuillEditorProps> = ({ value, onChange, ref}) => {
     }, [value]);
 
     return (
-        <div className="h-96 w-full border-1">
-            <div ref={editorRef} className="h-max-80 overflow-y-auto border border-gray-300 rounded-b-md"/>
+        <div className="h-96 pb-12 w-full">
+            <div ref={editorRef} className="h-full overflow-y-auto rounded-b-md"/>
             <div id="counter"/>
         </div>
     );
